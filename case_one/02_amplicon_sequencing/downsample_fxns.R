f8e974c75890fdb0be55ce5914508c53dd7b2689
Rebecca Eliscu <rebeliscu@gmail.com>
2023-06-15 11:33:27 -0700

    Remove functions that weren't ultimately used

--- case_one/02_amplicon_sequencing/downsample_fxns.R
+++ case_one/02_amplicon_sequencing/downsample_fxns.R
@@ -10,13 +10,13 @@ flexiblas_switch(2)
 options(future.globals.maxSize = +Inf)
 openblas_set_num_threads(1)
 
-downsampleCoverage <- function(projectName,
-                               readsPerSample, ## Paths to reads per sample (one file per sample)
-                               whichAmps,
-                               coverageIntervals,
-                               nResamples,
-                               nThreads,
-                               outDir) {
+downsampleSelfCov <- function(projectName,
+                              readsPerSample,
+                              whichAmp,
+                              coverageIntervals,
+                              nResamples,
+                              nThreads,
+                              outDir) {
   registerDoFuture()
   plan(multicore, workers = nThreads)
   for (coverage in coverageIntervals) {
@@ -25,39 +25,33 @@ downsampleCoverage <- function(projectName,
     for (sample in readsPerSample) {
       cat("\nStarting", sample, "\n")
       workingSample <- as.data.frame(fread(sample, nThread = nThreads))
-      downsample <- foreach(seq(nResamples), .combine = rbind) %dorng% {
-        ## Initialize downsample1 with first amp
-        workingAmp <- workingSample[workingSample[, 1] %in% whichAmps[1],]
-        replace <- FALSE
-        if (nrow(workingAmp) < coverage)
-          replace <- TRUE
-        downsample1 <- sample_n(workingAmp, coverage, replace = replace) 
-        ## Rest amps
-        for (amp in whichAmps[2:length(whichAmps)]) {
-          workingAmp <- workingSample[workingSample[, 1] %in% amp,]
-          replace <- FALSE
-          if(nrow(workingAmp) < coverage)
-            replace <- TRUE
-          downsample1 <- rbind(downsample1, sample_n(workingAmp, coverage, replace = replace)) ## Sample x reads per amplicon
-        }
-        ## Summarize number of alt and ref reads per amplicon:
+      workingSample <- workingSample[workingSample[, 1] %in% whichAmp,]
+      downsample <- foreach(seq(nResamples), .combine = cbind) %dorng% {
+        idx <- sample(seq(nrow(workingSample)), coverage*2) 
+        idx1 <- sample(idx, coverage)
+        idx2 <- idx[!idx %in% idx1]
+        downsample1 <- workingSample[idx1,]
+        downsample2 <- workingSample[idx2,]
         if(sum(downsample1$type %in% "alt") != 0) {
           downsample1 <- merge(as.data.frame(table(downsample1$gene[downsample1$type == "alt"])), as.data.frame(table(downsample1$gene[downsample1$type == "ref"])), by = 1, all = TRUE)
         } else {
-          downsample1 <- merge(data.frame(whichAmps, NA), as.data.frame(table(downsample1$gene[downsample1$type == "ref"])), by = 1, all = TRUE)
+          downsample1 <- merge(data.frame(whichAmp, NA), as.data.frame(table(downsample1$gene[downsample1$type == "ref"])), by = 1, all = TRUE)
+        }
+        if(sum(downsample2$type %in% "alt") != 0) {
+          downsample2 <- merge(as.data.frame(table(downsample2$gene[downsample2$type == "alt"])), as.data.frame(table(downsample2$gene[downsample2$type == "ref"])), by = 1, all = TRUE)
+        } else {
+          downsample2 <- merge(data.frame(whichAmp, NA), as.data.frame(table(downsample2$gene[downsample2$type == "ref"])), by = 1, all = TRUE)
         }
         downsample1[is.na(downsample1)] <- 0
-        colnames(downsample1) <- c("Amplicon", "Alt", "Ref")
-        return(downsample1)
+        downsample2[is.na(downsample2)] <- 0
+        colnames(downsample2) <- colnames(downsample1) <- c("Amplicon", "Alt", "Ref")
+        return(rbind(downsample1, downsample2))
       } # end downsamples
       downsampledCoverage[[sample]] <- downsample
       rm(downsample)
       collectGarbage()
     } # end samples
-    assign(paste0("downsample_coverage_", coverage, "x"), downsampledCoverage)
-
+    assign(paste0("downsample_self_coverage_", coverage, "x"), downsampledCoverage)
   } ## // end coverage intervals
-  save(list = ls()[grep("downsample_", ls())], file = paste0(outDir, "downsampleCoverage_", projectName, "_", nResamples, "_resamples.RData"))
-
-} ## // end fxn
-
+  save(list = ls()[grep("downsample_self_coverage_", ls())], file = paste0(outDir, "downsampleSelfCov_", projectName, "_", nResamples, "_resamples.RData"))
+} ## downsampleSelfCov()
